51f60f301cbb790229dbb5455da5fd05de8b8e67
andresarivera-cp1 <andresarivera@unicauca.edu.co>
2026-04-08 23:26:07 -0500

    script.js

--- script.js
+++ script.js
@@ -11,3 +11,21 @@ L.marker([2.4448, -76.6147])
   .addTo(map)
   .bindPopup("Probando desde GitHub Pages 🚀")
   .openPopup();
+
+// 🔥 Escuchar Firebase
+const ubicacionRef = window.ref(window.db, 'ubicacion');
+
+window.onValue(ubicacionRef, (snapshot) => {
+    const data = snapshot.val();
+
+    if (data) {
+        const lat = data.lat;
+        const lng = data.lng;
+
+        // mover marcador
+        marker.setLatLng([lat, lng]);
+
+        // mover el mapa
+        map.setView([lat, lng], 15);
+    }
+});
