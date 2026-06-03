b8aa6781f0554db9c7e803efb30a7d7e54650227
andresarivera-cp1 <andresarivera@unicauca.edu.co>
2026-06-03 00:56:34 -0500

    u6

--- script.js
+++ script.js
@@ -30,27 +30,27 @@ const lugares = [
     },
     {
         coords: [2.446918364817143, -76.59763764241958],
-        iconUrl: 'img/lugar1.png',
+        iconUrl: 'img/lugar2.png',
         popup: 'Facultad de Contables'
     },
     {
         coords: [2.4458887205340663, -76.59814801146317],
-        iconUrl: 'img/lugar1.png',
+        iconUrl: 'img/lugar3.png',
         popup: 'Facultad de Ingeniería civil'
     },
     {
         coords: [2.4463101213632137, -76.60043562986958],
-        iconUrl: 'img/lugar1.png',
+        iconUrl: 'img/lugar4.png',
         popup: 'Facultad de Educación'
     },
         {
         coords: [2.441524668419758, -76.6035057023519],
-        iconUrl: 'img/lugar1.png',
+        iconUrl: 'img/lugar5.png',
         popup: 'Facultad de Ciencias Sociales/Humanidades'
     },
     {
         coords: [2.4417364484593755, -76.60471300594388],
-        iconUrl: 'img/lugar1.png',
+        iconUrl: 'img/lugar6.png',
         popup: 'Facultad de Derecho'
     },
 ];
